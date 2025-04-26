b2e432dccc143004e1d6ee082e4e7f7762eb50fd
amrawey <amraweykhalifa@gmail.com>
2025-04-26 07:24:09 +0300

    renaming window.py to graphics.py

--- graphics.py
+++ graphics.py
@@ -17,7 +17,10 @@ class Window:
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
-    
+    def draw_line(self, line):
+        self.__canvas.delete("all")
+        line.draw(self.__canvas)
+        self.__canvas.update()
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -25,3 +28,15 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    def draw(self, canvas):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill="black", width=2)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from window import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    point_1 = Point(4,0)
+    point_2 = Point(4,100)
+    line = Line(point_1, point_2)
+    win.draw_line(line)
     win.wait_for_close()
 
 if __name__ == "__main__":
